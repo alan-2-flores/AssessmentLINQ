cc39e3e4a0d9ad081e0d83dc5d92215e47cddb73
Flores <alan.2.flores@atos.net>
2022-07-06 13:24:09 -0500

    assessment usando LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class queries {
+        public string[] ordenamientoDesc(String[] nombres) {
+            IEnumerable<string> result=from nombre in nombres
+                                        orderby nombre descending
+                                        select nombre;
+            return result.ToArray();
+        }
+        public bool coincidencia(String[] nombres, String objetivo) {
+            IEnumerable<string> result = from nombre in nombres
+                                         where nombre == objetivo
+                                         select nombre;
+            if (result.Any()) {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public string[] empiezaCon(String[] nombres)
+        {
+            IEnumerable<string> result = from nombre in nombres
+                                         where nombre.Contains("Juan")
+                                         select nombre;
+            return result.ToArray();
+        }
+
+        public string[] concatena(String[] nombres)
+        {
+            String [] result = new String[nombres.Length];
+            for (int i = 0; i < result.Length; i++) {
+                result[i] = "Hola " + nombres[i];
+            }
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nombres = { "Juan", "Pablo", "Pedro", "Pepe", "Ana", "Esteban", "Daniel", "Mariano", "Carolina", "Silvia", "Roberto", "Juanito", "Juana" };
+            var query = new queries();
+            String[] nombresDesc = query.ordenamientoDesc(nombres);
+            String[] nombresJuan = query.empiezaCon(nombres);
+            String[] holaNombres = query.concatena(nombres);
+
+            foreach (String nombre in nombresDesc) {
+                Console.WriteLine(nombre);
+            }
+
+            Console.WriteLine("\nEscribe el nombre a buscar:");
+            String objetivo=Console.ReadLine();
+            Console.WriteLine("\n" + query.coincidencia(nombres, objetivo) +"\n");
+
+            foreach (String nombre in nombresJuan)
+            {
+                Console.WriteLine(nombre);
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (String nombre in holaNombres)
+            {
+                Console.WriteLine(nombre);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
